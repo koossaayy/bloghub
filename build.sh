9a490a527177c2c20ba1210d4eb15db95913b161
calebdaasi <calebdassi@gmail.com>
2026-03-20 02:55:52 +0100

    suppression fichiers vercel

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Installer Composer
-curl -sS https://getcomposer.org/installer -o composer-setup.php
-php8.2 composer-setup.php --install-dir=/usr/local/bin --filename=composer
-composer install --no-dev --optimize-autoloader
-npm install
-npm run build
