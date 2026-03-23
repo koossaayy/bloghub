21ee7509b8dee6a48a7a709e3c245b82c11520f3
Koussay <6431084+koossaayy@users.noreply.github.com>
2026-03-23 01:12:04 +0100

    hellow rodl

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
